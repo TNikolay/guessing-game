8a8bcd794a373f42ba7be4f30e60d52e7633c797
Nikolay Tyushkov <begemot@begemotov.net>
2023-02-09 15:03:03 +0300

    feat: done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.cur = Math.round(min + (max - min) / 2);
     }
 
     guess() {
-
+        //console.log('---', this.min, this.max, this.cur);
+        return this.cur;
     }
 
     lower() {
-
+        this.max = this.cur ;
+        this.cur = Math.round(this.min + (this.cur - this.min) / 2);
     }
-
+    
     greater() {
-
+        this.min = this.cur ;
+        this.cur = Math.round(this.cur + (this.max - this.cur) / 2);
     }
 }
 
